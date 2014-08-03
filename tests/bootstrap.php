1a19c1b291c84d12bda6fea743db1c941642ab52
66d27b3ea53eda58ad7f71253dc2338b08efe4c4 <botrelli@gmx.com>
2014-08-03 10:26:08 -0400

    CS Fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,11 @@ if (is_file(__DIR__ . '/../vendor/autoload.php')) {
 
   require_once __DIR__ . '/../vendor/autoload.php';
 
-} else if (is_file(__DIR__ . '/../../../autoload.php')) {
+} elseif (is_file(__DIR__ . '/../../../autoload.php')) {
 
   require_once __DIR__ . '/../../../autoload.php';
 
-} else if (is_file(__DIR__ . '/../autoload.php')) {
+} elseif (is_file(__DIR__ . '/../autoload.php')) {
 
   require_once __DIR__ . '/../autoload.php';
 
